70038ce898d9b201c445a1175ce3e79b23881b0d
biocompt <biocompt@gmail.com>
2025-02-17 19:28:12 +0100

    updated to install RStudio

--- software/install_R.sh
+++ software/install_R.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-# R Installation and Essential Libraries Setup on Ubuntu
+# R Installation and Essential Libraries Setup on Ubuntu (22.04 LTS)
 # This file provides a step-by-step guide to install R (version 4.2.2) on Ubuntu, along with the essential system libraries required for installing and running R packages.
 # It ensures that R and its dependencies are properly configured, enabling seamless installation and usage of additional R packages.
 
 # 1. Install mandatory packages
 sudo apt-get update && sudo apt-get install \
 build-essential fort77 xorg-dev liblzma-dev libblas-dev gfortran \
-gcc-multilib gobjc++ aptitude \
+gcc-multilib gobjc++ aptitude libssl-dev libclang-dev \
 libreadline-dev libbz2-dev libpcre2-dev \
 libcurl4 libcurl4-openssl-dev \
 default-jre default-jdk openjdk-8-jdk openjdk-8-jre -y
@@ -24,4 +24,5 @@ make check
 sudo make install
 
 # 4. Optional: install Rstudio
-# Ubuntu 22: https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.12.1-563-amd64.deb. Then install with Software
+wget https://download1.rstudio.org/electron/jammy/amd64/rstudio-2024.12.1-563-amd64.deb
+sudo dpkg --install rstudio-2024.12.1-563-amd64.deb
